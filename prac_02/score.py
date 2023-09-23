e7b34d4d889e679e0314d9e566d07b980d28e253
jackk <jack.kerlin@my.jcu.edu.au>
2023-09-23 22:50:58 +1000

    Refactor the scores and temperatures program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,22 @@ CP1404 Prac 1 Jack Kerlin
 Fixed version of broken program
 """
 
-score = float(input("Enter score: "))
-if score > 100 or score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    print(check_score(score))
+    print(check_score(random.randint(0, 100)))
+def check_score(score):
+    if score > 100 or score < 0:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -3,22 +3,33 @@ CP1404 Prac 1 Jack Kerlin
 """
 
 
-MENU = """C - Convert Celsius to Fahrenheit
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = (fahrenheit - 32) * 5/9
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = convert_c_to_f()
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            celsius = convert_f_to_c()
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_f_to_c():
+    fahrenheit = float(input("Fahrenheit: "))
+    return (fahrenheit - 32) * 5 / 9
+
+def convert_c_to_f():
+    celsius = float(input("Celsius: "))
+    return celsius * 9.0 / 5 + 32
+
+
+main()
