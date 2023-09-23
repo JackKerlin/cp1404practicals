eb976e39ba2a785a221b90e49e7d5de674ce262e
jackk <jack.kerlin@my.jcu.edu.au>
2023-09-23 22:23:51 +1000

    Refactor password program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,10 +2,22 @@
 CP1404 Prac 2 Jack Kerlin
 """
 
-min_length = 5
 
-password = input("Password: ")
-while len(password) < min_length:
-    print("Invalid Password")
+def main():
+    password = get_password()
+    display_password(password)
+
+
+def display_password(password):
+    print("*" * len(password))
+
+
+def get_password():
+    min_length = 5
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < min_length:
+        print("Invalid Password")
+        password = input("Password: ")
+    return password
+
+main()
