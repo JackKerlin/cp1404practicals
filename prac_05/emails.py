c12acd539175ad27d3921b3124a03e1376ee74da
jackk <jack.kerlin@my.jcu.edu.au>
2023-10-13 15:28:21 +1100

    Add emails program

---
+++ prac_05/emails.py
@@ -0,0 +1,17 @@
+"""
+CP1404 Prac 5 Jack Kerlin
+Expected: 30 minutes
+Actual: 33 mins
+"""
+
+emails_to_name = {}
+email = input("Email: ")
+while email != "":
+    name = " ".join(email.split("@")[0].split(".")).title()
+    response = input(f"Is your name {name}? ").lower()
+    if response in ["", "y"]:
+        emails_to_name[email] = name
+    else:
+        emails_to_name[email] = input("Enter name: ")
+    email = input("Email: ")
+[print(f"{emails_to_name[email]} ({email})") for email in emails_to_name.keys()]
