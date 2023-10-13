0f1b4335ce19bc82d603b3e0fe76bc843974f463
jackk <jack.kerlin@my.jcu.edu.au>
2023-10-13 16:54:17 +1100

    Add constants and clean main func

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,29 +4,34 @@ Expected: 50 minutes
 Actual: 40 minutes
 """
 
+FILENAME = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
+
 
 def main():
-    with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
-        lines = format_file(in_file)
-        del lines[0]
-        champions_to_count = count_champions(lines)
-        print("Wimbledon Champions: ")
-        [print(f"{champion} {champions_to_count[champion]}") for champion in champions_to_count]
-        countries = count_countries(lines)
-        print(f"These {len(countries)} have won Wimbledon:\n" + ", ".join(countries))
+    lines = format_file()
+    del lines[0]  # delete header
+    champions_to_count = count_champions(lines)
+    print("Wimbledon Champions: ")
+    # list comp to replace for loop
+    [print(f"{champion} {champions_to_count[champion]}") for champion in champions_to_count]
+    countries = count_countries(lines)
+    print(f"These {len(countries)} have won Wimbledon:\n" + ", ".join(countries))
 
 
-def format_file(in_file):
-    lines = in_file.readlines()
-    # returns a list of lists
-    return [line.split(',') for line in lines]
+def format_file():
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        lines = in_file.readlines()
+        # returns a list of lists
+        return [line.split(',') for line in lines]
 
 
 def count_champions(lines):
     champions_to_count = {}
     # counts champs in the lines
     for line in lines:
-        champion = line[2]
+        champion = line[CHAMPION_INDEX]
         champions_to_count[champion] = champions_to_count.get(champion, 0) + 1
     return champions_to_count
 
@@ -34,7 +39,7 @@ def count_champions(lines):
 def count_countries(lines):
     countries = set()
     # adds countries to set, set automatically ignores duplicates
-    [countries.add(line[1]) for line in lines]
+    [countries.add(line[COUNTRY_INDEX]) for line in lines]
     return sorted(countries)
 
 
