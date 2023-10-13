8fedf4be5949ee13a3b6cca9d7df37676957583f
jackk <jack.kerlin@my.jcu.edu.au>
2023-10-13 20:42:13 +1100

    Fix error

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,7 +5,8 @@ CP1404 Prac 5 Jack Kerlin
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 
-print("\n".join([f"{state:<3} is {CODE_TO_NAME[state]}" for state in CODE_TO_NAME]))
+print("\n".join([f"{code:<3} is {CODE_TO_NAME[code]}" for code in CODE_TO_NAME]))
+
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
