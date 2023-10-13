9de86ddaa7d20392d84cf484517fa815a600277e
jackk <jack.kerlin@my.jcu.edu.au>
2023-10-13 15:07:46 +1100

    Add hex colours program

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Prac 5 Jack Kerlin
+"""
+
+COLOUR_TO_CODE = {"Acid Green": "#b0bf1a", "Aqua": "##00ffff", "Dark Violet": "#9400d3", "Denim": "#1560bd",
+                  "Eggplant": "#614051", "Green Yellow": "#adff2f", "Harlequin": "#3fff00", "Lilac": "#c8a2c8",
+                  "Linen": "#faf0e6"}
+
+colour = input("Enter colour: ").lower().title()
+print(colour)
+while colour != "":
+    try:
+        print(f"{colour} code is {COLOUR_TO_CODE[colour]}")
+    except KeyError:
+        print("Invalid colour.")
+    colour = input("Enter colour: ").lower().title()
--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -4,12 +4,9 @@ Estimate: 40 minutes
 Actual: 29 minutes
 """
 
-text = input("Text: ")
+words = input("Text: ").lower().split()
 word_to_number = {}
-for word in text.lower().split():
-    if word in word_to_number:
-        word_to_number[word] += 1
-    else:
-        word_to_number[word] = 1
-width = max([len(word) for word in word_to_number])+ 1
+for word in words:
+    word_to_number[word] = word_to_number.get(word, 0) + 1
+width = max([len(word) for word in word_to_number]) + 1
 [print(f"{word:{width}}: {word_to_number[word]}") for word in sorted(word_to_number.keys())]
