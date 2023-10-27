784ad824ac51e2dd7c8d51e5ede9fb5481660577
jackk <jack.kerlin@my.jcu.edu.au>
2023-10-27 17:53:26 +1100

    Fix printed index not starting at 1

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -18,14 +18,15 @@ while name != "":
     guitars.append(guitar)
     name = input("Name: ")
 
-guitars.append(Guitar("Gibson L-5 CES", 1922, 16035))
-guitars.append(Guitar("Line 6 JTV-59", 2010, 1512))
-guitars.append(Guitar("Fender Stratocaster", 2014, 765.40350340609346039))
+# guitars.append(Guitar("Gibson L-5 CES", 1922, 16035))
+# guitars.append(Guitar("Line 6 JTV-59", 2010, 1512))
+# guitars.append(Guitar("Fender Stratocaster", 2014, 765.40350340609346039))
+
 # find formatting width
 guitar_name_width = max([len(guitar.name) for guitar in guitars])
 # use f-string formatting as otherwise doesn't consider width change of rounding to 2 decimals
 guitar_cost_width = max([len(f"{guitar.cost:,.2f}") for guitar in guitars])
 for i, guitar in enumerate(guitars):
     vintage_string = " (vintage)" if guitar.is_vintage() else ""
-    print(f"Guitar {i}: {guitar.name:>{guitar_name_width}} ({guitar.year}), "
+    print(f"Guitar {i + 1}: {guitar.name:>{guitar_name_width}} ({guitar.year}), "
           f"worth ${guitar.cost:>{guitar_cost_width},.2f}{vintage_string}")
