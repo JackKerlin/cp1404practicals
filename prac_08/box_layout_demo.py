119951689a543d3cc79a063639ecbfaa8c1d3dce
jackk <jack.kerlin@my.jcu.edu.au>
2023-11-12 14:38:40 +1000

    Add doc files

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,15 +4,20 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Build kivy app from file"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def clear(self):
+        """Clear output and input."""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
     def handle_greet(self):
+        """Set output as greeting."""
         print("test")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+
 BoxLayoutDemo().run()
