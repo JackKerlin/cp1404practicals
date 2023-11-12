086549f5ec3f1d262009659243ded3c4c562e999
jackk <jack.kerlin@my.jcu.edu.au>
2023-11-12 14:38:55 +1000

    Add doc files

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -5,31 +5,40 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-
+from kivy.properties import StringProperty
 
 CONVERSION_FACTOR = 1.609344
 
-class MilesKmConversionApp(App):
+
+class MilesKmConverterApp(App):
     """Kivy app to convert miles to km"""
+    output_text = StringProperty()
 
     def build(self):
-        """ build the Kivy app """
+        """Build kivy app from file"""
         Window.size = (200, 100)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_increment(self, value, increment_value):
+    def handle_increment(self, text, increment_value):
+        """Increment input text by some value"""
+        miles = self.convert_to_float(text)
+        self.root.ids.input_miles.text = str(miles + increment_value)
+
+    def handle_conversion(self, text):
+        """Set output as calculated result"""
+        miles = self.convert_to_float(text)
+        result = miles * CONVERSION_FACTOR
+        self.output_text = str(result)
+
+    @staticmethod
+    def convert_to_float(text):
+        """Convert the text to a float"""
         try:
-            self.root.ids.input_miles.text = str(float(value) + increment_value)
+            return float(text)
         except ValueError:
-            self.root.ids.input_miles.text = str(increment_value)
-    def handle_conversion(self, value):
-        try:
-            result = float(value) * CONVERSION_FACTOR
-            self.root.ids.output_label.text = str(result)
-        except ValueError:
-            self.root.ids.output_label.text = "0.0"
+            return 0.0
 
 
-MilesKmConversionApp().run()
+MilesKmConverterApp().run()
