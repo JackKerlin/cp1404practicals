9ebf6987e762d1672091b4739123a889a4374220
jackk <jack.kerlin@my.jcu.edu.au>
2023-11-19 16:40:58 +1000

    Update unreliable car test

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -4,13 +4,18 @@ Car class
 """
 from unreliable_car import UnreliableCar
 
-# Test 100% reliable car
-reliable_car = UnreliableCar("Reliable Car", 1000, 100)
-reliable_car.drive(100)
-print(reliable_car)
-assert reliable_car.fuel == 900
-# Test 0% reliable car
-unreliable_car = UnreliableCar("Unreliable Car", 1000, 0)
-unreliable_car.drive(100)
-print(unreliable_car)
-assert unreliable_car.fuel == 1000
+# Test 95% reliable car
+reliable_car = UnreliableCar("Reliable Car", 1000, 95)
+# Test 5% reliable car
+unreliable_car = UnreliableCar("Unreliable Car", 1000, 5)
+# Test 50% reliable car
+semireliable_car = UnreliableCar("Semi-reliable Car", 1000, 50)
+test_count = [0, 0, 0]
+for i in range(0, 100):
+    if reliable_car.drive(1) == 1:
+        test_count[0] += 1
+    if unreliable_car.drive(1) == 1:
+        test_count[1] += 1
+    if semireliable_car.drive(1) == 1:
+        test_count[2] += 1
+print(test_count)
