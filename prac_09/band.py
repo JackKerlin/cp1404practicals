c409b2565c6fd873d054bfc89c2dbb27ad5e8f70
jackk <jack.kerlin@my.jcu.edu.au>
2023-11-19 14:43:47 +1000

    Add band class

---
+++ prac_09/band.py
@@ -0,0 +1,28 @@
+"""
+CP1404 Practical Jack Kerlin
+Band class
+"""
+
+
+class Band:
+    """Represent a Band object"""
+
+    def __init__(self, name):
+        """Initialise a Guitar."""
+        self.name = name
+        self.musicians = []
+
+    def __repr__(self):
+        return str(vars(self))
+    def __str__(self):
+        musicians_string = ", ".join(str(musician) for musician in self.musicians)
+        return f"{self.name} ({musicians_string})"
+
+    def add(self, musician):
+        self.musicians.append(musician)
+
+    def play(self):
+        play_string = []
+        for musician in self.musicians:
+            play_string.append(musician.play())
+        return "\n".join(play_string)
