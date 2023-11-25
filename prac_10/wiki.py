77296f2d21a91cb57fc2af83b634b16060c9004d
jackk <jack.kerlin@my.jcu.edu.au>
2023-11-26 02:26:32 +1000

    Add wikipedia program

---
+++ prac_10/wiki.py
@@ -0,0 +1,22 @@
+"""
+CP1404 Practical Jack Kerlin
+Wiki api
+Estimate: 40 minutes
+Actual: 30 minutes
+"""
+
+import wikipedia
+print(wikipedia.languages())
+
+user_search = input("Enter Wikipedia search: ")
+while user_search != "":
+    try:
+        searched_page = wikipedia.page(user_search, auto_suggest=False)
+        print(searched_page.title)
+        print(searched_page.summary)
+        print(searched_page.url)
+    except wikipedia.exceptions.DisambiguationError as disambiguation:
+        print(disambiguation.options)
+    except wikipedia.exceptions.PageError:
+        print("Page not found :(")
+    user_search = input("Enter Wikipedia search: ")
